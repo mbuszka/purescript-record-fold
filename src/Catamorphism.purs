5b2a2b9e9841e64ad89dd27de2d7d5516723b427
Maciej Buszka <maciej@buszka.eu>
2018-04-03 10:34:47 +0200

    Catamorphism can change type of accumulator
    - Implementation
    - Example with mapJust

--- src/Catamorphism.purs
+++ src/Catamorphism.purs
@@ -3,45 +3,83 @@ module Catamorphism where
 import Prelude
 
 import Data.Array (cons)
-import Data.Record (delete, get)
+import Data.Maybe (Maybe(..))
+import Data.Record (get, insert)
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 import Data.Tuple (Tuple(..))
-import Type.Row (class ListToRow, class RowLacks, class RowToList, Cons, Nil, RLProxy(..), kind RowList)
+import Type.Row (class RowLacks, class RowToList, Cons, Nil, RLProxy(..), kind RowList)
 
-class Algebra (name :: Symbol) (input :: Type) (res :: Type) | name -> res where
-  algebra :: forall sym. IsSymbol sym => (SProxy name) -> (SProxy sym) -> input -> res -> res
+class Algebra (name :: Symbol) (lbl :: Symbol) input res' res | name -> res where
+  algebra :: (SProxy name) -> (SProxy lbl) -> input -> res' -> res
+
+class Zero (name :: Symbol) res | name -> res where
+  algZero :: SProxy name -> res
 
 class Cata (name :: Symbol) (list :: RowList) (row :: # Type) res | name -> res where
-  cata :: (SProxy name) -> (RLProxy list) -> res -> Record row -> res
+  cata :: (SProxy name) -> (RLProxy list) -> Record row -> res
 
 instance cataRowCons
   ::
-  ( Algebra name input res
+  ( Algebra name lbl input res' res
   , IsSymbol lbl
   , RowCons lbl input rest row
-  , Cata name tail row res
+  , Cata name tail row res'
   ) => Cata name (Cons lbl input tail) row res where
-  cata name _ zero record =
+  cata name _ record =
     let
       key = SProxy :: SProxy lbl
       tail = RLProxy :: RLProxy tail
-      res = cata name tail zero record
+      res = cata name tail record
     in algebra name key (get key record) res
 
 instance cataRowNil
-  :: Cata name Nil r res where
-  cata _ _ zero _ = zero
+  :: Zero name res => Cata name Nil r res where
+  cata name _ _ = algZero name
 
-instance algebraLen :: Algebra "length" a Int where
+instance algebraLen :: Algebra "length" lbl a Int Int where
   algebra _ _ _ c = c + 1
 
-recordLen :: forall row list. RowToList row list => Cata "length" list row Int => Record row -> Int
-recordLen rec = cata (SProxy :: SProxy "length") (RLProxy :: RLProxy list) 0 rec
+instance zeroLen :: Zero "length" Int where
+  algZero _ = 0
+
+recordLen
+  :: forall row list
+   . RowToList row list
+  => Cata "length" list row Int
+  => Record row -> Int
+recordLen rec = cata (SProxy :: SProxy "length") (RLProxy :: RLProxy list) rec
 
 type Res = Array (Tuple String String)
 
-instance algebraShow :: Show a => Algebra "show" a (Array (Tuple String String)) where
+instance algebraShow ::
+  ( Show a
+  , IsSymbol lbl
+  ) => Algebra "show" lbl a (Array (Tuple String String)) (Array (Tuple String String)) where
   algebra _ sym val acc = cons (Tuple (reflectSymbol sym) (show val)) acc
 
-recordShow :: forall row list. RowToList row list => Cata "show" list row Res => Record row -> Res
-recordShow rec = cata (SProxy :: SProxy "show") (RLProxy :: RLProxy list) [] rec
+instance zeroShow :: Zero "show" (Array (Tuple String String)) where
+  algZero _ = []
+
+recordShow
+  :: forall row list
+   . RowToList row list
+  => Cata "show" list row Res
+  => Record row -> Res
+recordShow rec = cata (SProxy :: SProxy "show") (RLProxy :: RLProxy list) rec
+
+instance algebraMapJust ::
+  ( IsSymbol lbl
+  , RowCons lbl (Maybe a) tail row
+  , RowLacks lbl tail
+  ) => Algebra "mapJust" lbl a (Record tail) (Record row) where
+  algebra _ lbl val acc = insert lbl (Just val) acc
+
+instance zeroMapJust :: Zero "mapJust" (Record ()) where
+  algZero _ = {}
+
+recordMapJust
+  :: forall row list res
+   . RowToList row list
+  => Cata "mapJust" list row res
+  => Record row -> res
+recordMapJust r = cata (SProxy :: SProxy "mapJust") (RLProxy :: RLProxy list) r
--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,14 @@
 module Main where
 
 import Prelude
+
+import Catamorphism (recordLen, recordMapJust, recordShow)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Catamorphism
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log "Hello sailor!"
   log $ show $ recordLen { a: 123, b: "hello"}
   log $ show $ recordShow { a: 123, b: "hello"}
+  log $ show $ recordShow $ recordMapJust { a: 123, b: "hello"}
