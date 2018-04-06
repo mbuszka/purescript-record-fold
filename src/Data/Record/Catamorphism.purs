140273647bff2319246fef6985ad1545e481a131
Maciej Buszka <maciej@buszka.eu>
2018-04-06 16:52:43 +0200

    Runtime Algebra WIP
    - Algebra is now a datatype for which type class is instantiated.
      It allows us to pass constants, functions etc

--- src/Data/Record/Catamorphism.purs
+++ src/Data/Record/Catamorphism.purs
@@ -9,20 +9,20 @@ import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 import Data.Tuple (Tuple(..))
 import Type.Row (class RowLacks, class RowToList, Cons, Nil, RLProxy(..), kind RowList)
 
-class Algebra (name :: Symbol) (lbl :: Symbol) val res | name val -> res where
-  algebra :: (SProxy name) -> (SProxy lbl) -> val -> res
+class Algebra alg (lbl :: Symbol) val res | val -> res where
+  algebra :: alg -> (SProxy lbl) -> val -> res
 
-class Cata (name :: Symbol) (list :: RowList) (row :: # Type) res | name -> res where
-  cata :: (SProxy name) -> (RLProxy list) -> Record row -> res
+class Cata alg (list :: RowList) (row :: # Type) res | list -> res where
+  cata :: alg -> (RLProxy list) -> Record row -> res
 
 instance cataRowCons
   ::
-  ( Algebra name lbl val (fun a b)
+  ( Algebra alg lbl val (fun a b)
   , IsSymbol lbl
   , RowCons lbl val rest row
   , Semigroupoid fun
-  , Cata name tail row (fun b c)
-  ) => Cata name (Cons lbl val tail) row (fun a c) where
+  , Cata alg tail row (fun b c)
+  ) => Cata alg (Cons lbl val tail) row (fun a c) where
   cata name _ record =
     let
       key = SProxy :: SProxy lbl
@@ -34,99 +34,104 @@ instance cataRowNil
   :: (Category fun) => Cata name Nil r (fun a a) where
   cata name _ _ = id
 
-instance algebraLen :: Algebra "length" lbl a (Int -> Int) where
+data LenAlgebra = LenAlgebra
+
+instance lenAlgebra :: Algebra LenAlgebra lbl a (Int -> Int) where
   algebra _ _ _ z = z + 1
+
+-- instance algebraLen :: Algebra "length" lbl a (Int -> Int) where
+--   algebra _ _ _ z = z + 1
 
 recordLen
   :: forall row list
    . RowToList row list
-  => Cata "length" list row (Int -> Int)
+  => Cata LenAlgebra list row (Int -> Int)
   => Record row -> Int
-recordLen rec = cata (SProxy :: SProxy "length") (RLProxy :: RLProxy list) rec $ 0
+recordLen rec = cata LenAlgebra (RLProxy :: RLProxy list) rec $ 0
 
 type Res = Array (Tuple String String)
+
+data ShowAlgebra = ShowAlgebra
 
 instance algebraShow ::
   ( Show a
   , IsSymbol lbl
-  ) => Algebra "show" lbl a ((Array (Tuple String String)) -> (Array (Tuple String String))) where
+  ) => Algebra ShowAlgebra lbl a ((Array (Tuple String String)) -> (Array (Tuple String String))) where
   algebra _ sym val acc = cons (Tuple (reflectSymbol sym) (show val)) acc
 
 recordShow
   :: forall row list
    . RowToList row list
-  => Cata "show" list row (Res -> Res)
+  => Cata ShowAlgebra list row (Res -> Res)
   => Record row -> Res
-recordShow rec = cata (SProxy :: SProxy "show") (RLProxy :: RLProxy list) rec $ []
+recordShow rec = cata ShowAlgebra (RLProxy :: RLProxy list) rec $ []
 
-{- This algebra allows us to insert each value into functorial container -}
+-- {- This algebra allows us to insert each value into functorial container -}
+data MapAlgebra (f :: Type -> Type) = MapAlgebra (forall a. a -> f a)
+
 instance algebraMap ::
   ( IsSymbol lbl
   , RowCons lbl (f a) tail row
   , RowLacks lbl tail
-  , Functor f
-  ) => Algebra "map" lbl a (Tuple (f Unit) (Record tail) -> Tuple (f Unit) (Record row)) where
-  algebra _ lbl val (Tuple c acc) = Tuple c $ insert lbl (map (const val) c) acc
+  ) => Algebra (MapAlgebra f) lbl a ((Record tail) -> (Record row)) where
+  algebra (MapAlgebra f) lbl val acc = insert lbl (f val) acc
 
 recordMap
-  :: forall f a row list res
+  :: forall f row list res
    . RowToList row list
-  => Functor f
-  => Cata "map" list row (Tuple (f Unit) {} -> Tuple (f Unit) res)
-  => f a -> Record row -> res
-recordMap proxy r =
+  => Cata (MapAlgebra f) list row ({} -> res)
+  => (forall a. a -> f a) -> Record row -> res
+recordMap f r =
   let
-    name = SProxy :: SProxy "map"
     list = RLProxy :: RLProxy list
-    acc = Tuple (map (const unit) proxy) {}
-    Tuple _ res = cata name list r acc
+    res = cata (MapAlgebra f) list r $ {}
   in res
 
-recordMapJust
-  :: forall row list res
-   . RowToList row list
-  => Cata "map" list row (Tuple (Maybe Unit) {} -> Tuple (Maybe Unit) res)
-  => Record row -> res
-recordMapJust = recordMap (Just 1)
+-- recordMapJust
+--   :: forall row list res
+--    . RowToList row list
+--   => Cata "map" list row (Tuple (Maybe Unit) {} -> Tuple (Maybe Unit) res)
+--   => Record row -> res
+-- recordMapJust = recordMap (Just 1)
+
+data ApplyAlgebra a = ApplyAlgebra a
 
 instance algebraApply ::
   ( IsSymbol lbl
   , RowCons lbl b tail row
   , RowLacks lbl tail
-  ) => Algebra "apply" lbl (a -> b) (Tuple a (Record tail) -> Tuple a (Record row)) where
-  algebra _ lbl f (Tuple c acc) = Tuple c $ insert lbl (f c) acc
+  ) => Algebra (ApplyAlgebra a) lbl (a -> b) ((Record tail) -> (Record row)) where
+  algebra (ApplyAlgebra c) lbl f acc = insert lbl (f c) acc
 
 recordApplyTo
   :: forall a row list res
    . RowToList row list
-  => Cata "apply" list row (Tuple a {} -> Tuple a res)
+  => Cata (ApplyAlgebra a) list row ({} -> res)
   => a -> Record row -> res
 recordApplyTo v r =
   let
-    name = SProxy :: SProxy "apply"
     list = RLProxy :: RLProxy list
-    acc = Tuple v {}
-    Tuple _ res = cata name list r acc
+    res = cata (ApplyAlgebra v) list r {}
   in res
 
-instance algebraCollect ::
-  ( IsSymbol lbl
-  , RowCons lbl a tail row
-  , RowLacks lbl tail
-  , Apply f
-  ) => Algebra "collect" lbl (f a) (f (Record tail) -> (f (Record row))) where
-  algebra _ lbl a acc = (insert lbl) <$> a <*> acc
+-- instance algebraCollect ::
+--   ( IsSymbol lbl
+--   , RowCons lbl a tail row
+--   , RowLacks lbl tail
+--   , Apply f
+--   ) => Algebra "collect" lbl (f a) (f (Record tail) -> (f (Record row))) where
+--   algebra _ lbl a acc = (insert lbl) <$> a <*> acc
 
-recordCollect
-  :: forall f row list res
-   . RowToList row list
-  => Applicative f
-  => Cata "collect" list row (f {} -> f res)
-  => Record row -> f res
-recordCollect r =
-  let
-    name = SProxy :: SProxy "collect"
-    list = RLProxy :: RLProxy list
-    acc = pure {}
-    res = cata name list r $ acc
-  in res
+-- recordCollect
+--   :: forall f row list res
+--    . RowToList row list
+--   => Applicative f
+--   => Cata "collect" list row (f {} -> f res)
+--   => Record row -> f res
+-- recordCollect r =
+--   let
+--     name = SProxy :: SProxy "collect"
+--     list = RLProxy :: RLProxy list
+--     acc = pure {}
+--     res = cata name list r $ acc
+--   in res
