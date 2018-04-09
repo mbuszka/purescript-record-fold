1c4b344372c1b70fcb282095a9532aec26739e21
Maciej Buszka <maciej@buszka.eu>
2018-04-09 10:46:35 +0200

    Change names of classes
    - Cata -> Fold
    - Algebra -> Step

--- src/Data/Record/Catamorphism.purs
+++ src/Data/Record/Catamorphism.purs
@@ -1,4 +1,4 @@
-module Data.Record.Catamorphism where
+module Data.Record.Fold where
 
 import Prelude
 
@@ -10,124 +10,124 @@ import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 import Data.Tuple (Tuple(..))
 import Type.Row (class RowLacks, class RowToList, Cons, Nil, RLProxy(..), kind RowList)
 
-class Algebra alg (lbl :: Symbol) val res | val -> res where
-  algebra :: alg -> (SProxy lbl) -> val -> res
+class Step stepper (lbl :: Symbol) val step | val -> step where
+  step :: stepper -> (SProxy lbl) -> val -> step
 
-class Cata alg (list :: RowList) (row :: # Type) res | list -> res where
-  cata :: alg -> (RLProxy list) -> Record row -> res
+class Fold stepper (list :: RowList) (row :: # Type) fold | list -> fold where
+  fold :: stepper -> (RLProxy list) -> Record row -> fold
 
-instance cataRowCons
+instance foldRowCons
   ::
-  ( Algebra alg lbl val (fun a b)
+  ( Step stepper lbl val (step a b)
   , IsSymbol lbl
   , RowCons lbl val rest row
-  , Semigroupoid fun
-  , Cata alg tail row (fun b c)
-  ) => Cata alg (Cons lbl val tail) row (fun a c) where
-  cata name _ record =
+  , Semigroupoid step
+  , Fold stepper tail row (step b c)
+  ) => Fold stepper (Cons lbl val tail) row (step a c) where
+  fold name _ record =
     let
       key = SProxy :: SProxy lbl
       tail = RLProxy :: RLProxy tail
-      res = cata name tail record
-    in algebra name key (get key record) >>> res
+      res = fold name tail record
+    in step name key (get key record) >>> res
 
-instance cataRowNil
-  :: (Category fun) => Cata name Nil r (fun a a) where
-  cata name _ _ = id
+instance foldRowNil
+  :: (Category step) => Fold name Nil r (step a a) where
+  fold name _ _ = id
 
-data LenAlgebra = LenAlgebra
+data LenStep = LenStep
 
-instance lenAlgebra :: Algebra LenAlgebra lbl a (Int -> Int) where
-  algebra _ _ _ c = c + 1
+instance lenStep :: Step LenStep lbl a (Int -> Int) where
+  step _ _ _ c = c + 1
 
 recordLen
   :: forall row list
    . RowToList row list
-  => Cata LenAlgebra list row (Int -> Int)
+  => Fold LenStep list row (Int -> Int)
   => Record row -> Int
-recordLen rec = cata LenAlgebra (RLProxy :: RLProxy list) rec $ 0
+recordLen rec = fold LenStep (RLProxy :: RLProxy list) rec $ 0
 
 type Res = Array (Tuple String String)
 
-data ShowAlgebra = ShowAlgebra
+data ShowStep = ShowStep
 
-instance algebraShow ::
+instance stepShow ::
   ( Show a
   , IsSymbol lbl
-  ) => Algebra ShowAlgebra lbl a ((Array (Tuple String String)) -> (Array (Tuple String String))) where
-  algebra _ sym val acc = cons (Tuple (reflectSymbol sym) (show val)) acc
+  ) => Step ShowStep lbl a ((Array (Tuple String String)) -> (Array (Tuple String String))) where
+  step _ sym val acc = cons (Tuple (reflectSymbol sym) (show val)) acc
 
 recordShow
   :: forall row list
    . RowToList row list
-  => Cata ShowAlgebra list row (Res -> Res)
+  => Fold ShowStep list row (Res -> Res)
   => Record row -> Res
-recordShow rec = cata ShowAlgebra (RLProxy :: RLProxy list) rec $ []
+recordShow rec = fold ShowStep (RLProxy :: RLProxy list) rec $ []
 
-data MapAlgebra (f :: Type -> Type) = MapAlgebra (forall a. a -> f a)
+data MapStep (f :: Type -> Type) = MapStep (forall a. a -> f a)
 
-instance algebraMap ::
+instance stepMap ::
   ( IsSymbol lbl
   , RowCons lbl (f a) tail row
   , RowLacks lbl tail
-  ) => Algebra (MapAlgebra f) lbl a (Builder (Record tail) (Record row)) where
-  algebra (MapAlgebra f) lbl val = B.insert lbl (f val)
+  ) => Step (MapStep f) lbl a (Builder (Record tail) (Record row)) where
+  step (MapStep f) lbl val = B.insert lbl (f val)
 
 recordMap
   :: forall f row list res
    . RowToList row list
-  => Cata (MapAlgebra f) list row (Builder {} (Record res))
+  => Fold (MapStep f) list row (Builder {} (Record res))
   => (forall a. a -> f a) -> Record row -> Record res
 recordMap f r =
   let
     list = RLProxy :: RLProxy list
     builder :: Builder {} (Record res)
-    builder = cata (MapAlgebra f) list r
+    builder = fold (MapStep f) list r
     res :: Record res
     res = build builder {}
   in res
 
 -- TODO Change to record builders
 
-data ApplyAlgebra a = ApplyAlgebra a
+data ApplyStep a = ApplyStep a
 
-instance algebraApply ::
+instance stepApply ::
   ( IsSymbol lbl
   , RowCons lbl b tail row
   , RowLacks lbl tail
-  ) => Algebra (ApplyAlgebra a) lbl (a -> b) ((Record tail) -> (Record row)) where
-  algebra (ApplyAlgebra c) lbl f acc = insert lbl (f c) acc
+  ) => Step (ApplyStep a) lbl (a -> b) ((Record tail) -> (Record row)) where
+  step (ApplyStep c) lbl f acc = insert lbl (f c) acc
 
 recordApplyTo
   :: forall a row list res
    . RowToList row list
-  => Cata (ApplyAlgebra a) list row ({} -> res)
+  => Fold (ApplyStep a) list row ({} -> res)
   => a -> Record row -> res
 recordApplyTo v r =
   let
     list = RLProxy :: RLProxy list
-    res = cata (ApplyAlgebra v) list r {}
+    res = fold (ApplyStep v) list r {}
   in res
 
-data CollectAlgebra = CollectAlgebra
+data CollectStep = CollectStep
 
-instance algebraCollect ::
+instance stepCollect ::
   ( IsSymbol lbl
   , RowCons lbl a tail row
   , RowLacks lbl tail
   , Apply f
-  ) => Algebra CollectAlgebra lbl (f a) (f (Record tail) -> (f (Record row))) where
-  algebra _ lbl a acc = (insert lbl) <$> a <*> acc
+  ) => Step CollectStep lbl (f a) (f (Record tail) -> (f (Record row))) where
+  step _ lbl a acc = (insert lbl) <$> a <*> acc
 
 recordCollect
   :: forall f row list res
    . RowToList row list
   => Applicative f
-  => Cata CollectAlgebra list row (f {} -> f res)
+  => Fold CollectStep list row (f {} -> f res)
   => Record row -> f res
 recordCollect r =
   let
     list = RLProxy :: RLProxy list
     acc = pure {}
-    res = cata CollectAlgebra list r $ acc
+    res = fold CollectStep list r $ acc
   in res
--- test/Main.purs
+++ test/Main.purs
@@ -2,7 +2,7 @@ module Test.Main where
 
 import Prelude
 
-import Data.Record.Catamorphism (recordApplyTo, recordCollect, recordLen, recordMap, recordShow)
+import Data.Record.Fold (recordApplyTo, recordCollect, recordLen, recordMap, recordShow)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Maybe (Maybe(..))
